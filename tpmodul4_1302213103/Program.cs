469204fcc6745dfc721f7877a9e656ca253101a7
RIPO12345 <placida.ripo.naibaho@gmail.com>
2023-03-12 15:08:37 +0700

    menambahkan implementasi dengan table-driven

--- tpmodul4_1302213103/Program.cs
+++ tpmodul4_1302213103/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class KodePos
+{
+    private Dictionary<string, string> tabelKodePos = new Dictionary<string, string>
+    {
+        {"Batununggal", "40266"},
+        {"A. Kujangsari", "40287"},
+        {"Mengger", "40267"},
+        {"Wates", "40256"},
+        {"Cijaura", "40287"},
+        {"Jatisari", "40286"},
+        {"Margasari", "40286"},
+        {"Sekejati", "40286"},
+        {"Kebonwaru", "40272"},
+        {"Maleer", "40274"},
+        {"Samoja", "40273"}
+    };
+
+    public string getKodePos(string kelurahan)
+    {
+        if (tabelKodePos.ContainsKey(kelurahan))
+        {
+            return tabelKodePos[kelurahan];
+        }
+        else
+        {
+            return "Kode pos tidak ditemukan.";
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodePos kodepos = new KodePos();
+
+        Console.WriteLine(kodepos.getKodePos("Batununggal")); // Output: 40266
+        Console.WriteLine(kodepos.getKodePos("A. Kujangsari")); // Output: 40287
+        Console.WriteLine(kodepos.getKodePos("Mengger")); // Output: 40267
+        Console.WriteLine(kodepos.getKodePos("Wates")); // Output: 40256
+        Console.WriteLine(kodepos.getKodePos("Cijaura")); // Output: 40287
+        Console.WriteLine(kodepos.getKodePos("Jatisari")); // Output: 40286
+        Console.WriteLine(kodepos.getKodePos("Margasari")); // Output: 40286
+        Console.WriteLine(kodepos.getKodePos("Sekejati")); // Output: 40286
+        Console.WriteLine(kodepos.getKodePos("Kebonwaru")); // Output: 40272
+        Console.WriteLine(kodepos.getKodePos("Maleer")); // Output: 40274
+        Console.WriteLine(kodepos.getKodePos("Samoja")); // Output: 40273
+        Console.WriteLine(kodepos.getKodePos("Cilaki")); // Output: Kode pos tidak ditemukan.
+    }
+}
